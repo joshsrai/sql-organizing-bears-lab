705b8266cc8a57870ea5692b7de2626da533ce46
joshsrai <joshsrai@gmail.com>
2020-11-16 15:27:50 -0500

    commit tree 438a1a59403724140e9fcd6fbda0321a0351951a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 22, 'M', 'brown', 'sweet', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 29, 'F', 'brown', 'rough', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 18, 'F', 'brown-greenish', 'goofy', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 53, 'M', 'grey', 'stern', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 35, 'F', 'black', 'playful', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 13, 'M', 'green', 'mean', '2');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 43, 'F', 'brown', 'kind', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'M', 'black', 'mean', '2'); 
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,41 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE sex = 'F'; "
+  #show names and ages from the bears table where sex = 'F'
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name FROM bears ORDER BY name;"
+  #show the names from the bears table in alphabetical order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age; "
+  #show the names and ages from the bears table that are alive and order them by age
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1; "
+  #show the name and age from the bears table of the oldest bear.
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears ORDER BY age LIMIT 1; "
+  #show the name and age from the bears table of the youngest bear.
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(COLOR) DESC LIMIT 1;"
+  #show the count of the color from the bearstable of most prominent color and limit it to 1
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy'; "
+  #show all the bears from the bears table that have temperament = 'goofy'
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name is NULL; "
+  #the bear that killed Tim does not have a name
+  #show all the bears from the bears table that killed Tim
+  
 end
